302860846616df4a90512708897115c528983596
jaehyucklim <jhlim087@gmail.com>
2026-02-15 14:28:45 +0900

    feat: add NamedQuery to Member entity

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -12,6 +12,10 @@ import static jakarta.persistence.FetchType.*;
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
