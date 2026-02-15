05b02ba60a30c26e81863f602ecbaa5dfa6f1b1b
jaehyucklim <jhlim087@gmail.com>
2026-02-15 14:52:44 +0900

    feat: add DTO projection query and  test

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -15,6 +17,7 @@ import static org.assertj.core.api.Assertions.*;
 class MemberRepositoryTest {
     
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
     
     @Test
     public void testMember() {
@@ -106,6 +109,22 @@ class MemberRepositoryTest {
         List<String> result = memberRepository.findUsernameList();
         for (String s : result) {
             System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+
+        List<MemberDto> result = memberRepository.findMemberDto();
+        for (MemberDto dto : result) {
+            System.out.println("dto = " + dto);
         }
     }
 }
