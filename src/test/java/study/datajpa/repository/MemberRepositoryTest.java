9f12f6f99e76e21adbb76fc2ddee1390a9e885c8
jaehyucklim <jhlim087@gmail.com>
2026-02-16 12:04:15 +0900

    feat: implement pessimistic write lock query using @Lock

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -253,4 +253,17 @@ class MemberRepositoryTest {
 
         em.flush(); // dirty checking -> update
     }
+
+    @Test
+    public void lock() {
+        // given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+
+    }
 }
